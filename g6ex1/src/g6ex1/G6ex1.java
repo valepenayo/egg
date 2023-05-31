f3dd0fdb1c261fb12b6ae5042879f0d00ccb7128
valepenayo <129123365+valepenayo@users.noreply.github.com>
2023-05-31 17:37:07 -0300

    g6ex1

---
+++ g6ex1/src/g6ex1/G6ex1.java
@@ -0,0 +1,15 @@
+/*
+ */
+package g6ex1;
+
+import entidad.cancion;
+import java.util.Scanner;
+
+public class G6ex1 {
+
+    public static void main(String[] args) {
+       Scanner leer= new Scanner(System.in);
+        cancion c1= new cancion(leer.nextLine(), leer.nextLine());
+    }
+    
+}
